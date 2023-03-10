f1b3e7eb6073683a0ff7eb234ff58334ab7e0abe
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-10 10:20:16 +0100

    finding the prime number, including 0 and 1.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
 public class Main {
     public static void main(String[] args) {
       int i, num, count=0;
-      num = 5;
-      for (i=2; i<num; i++){
-          if(num%i==0){
-              count++;
-              break;
+      num = 13;
+      if(num==0 || num==1){        // 0 and 1 are not prime number. we have to mention here
+          System.out.println("Not primitive");
+      }else {
+          for (i = 2; i < num/2; i++) {  //we are counting (num/2) half of 13. it's enough for finding prime num.
+              if (num % i == 0) {
+                  count++;
+                  break;
+              }
           }
-      }
-      if(count==0){
-          System.out.println("Prime number");
-      }else{
-          System.out.println("Not prime");
+          if (count == 0) {
+              System.out.println("Prime number");
+          } else {
+              System.out.println("Not prime");
+          }
       }
     }
 }
